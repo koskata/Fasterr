934a29e661b1272c0ef4441128fa5d41d344d9aa
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-06-04 14:03:14 +0300

    created class for validating entities

---
+++ Fasterr.Common/EntityValidationConstants.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fasterr.Common
+{
+    public static class EntityValidationConstants
+    {
+        public static class ApplicationUser
+        {
+            public const int FirstNameMinLength = 2;
+            public const int FirstNameMaxLength = 25;
+
+            public const int LastNameMinLength = 2;
+            public const int LastNameMaxLength = 25;
+        }
+
+        public static class Product
+        {
+            public const int NameMinLength = 2;
+            public const int NameMaxLength = 40;
+
+            public const int DescriptionMinLength = 10;
+            public const int DescriptionMaxLength = 250;
+        }
+    }
+}
