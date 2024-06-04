4980501847bc315cced26fca758b5fe02ec3b2f1
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-06-04 14:02:16 +0300

    Created first data models

---
+++ Fasterr.Data.Models/ApplicationUser.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+using static Fasterr.Common.EntityValidationConstants.ApplicationUser;
+
+namespace Fasterr.Data.Models
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+        public ApplicationUser()
+        {
+            Cart = new HashSet<ProductBuyerCart>();
+            PurchasedProducts = new HashSet<ProductBuyerPurchased>();
+        }
+
+        [Required]
+        [Comment("User first name")]
+        [StringLength(FirstNameMaxLength)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [Comment("User last name")]
+        [StringLength(LastNameMaxLength)]
+        public string LastName { get; set; } = string.Empty;
+
+        public virtual ICollection<ProductBuyerCart> Cart { get; set; }
+
+        public virtual ICollection<ProductBuyerPurchased> PurchasedProducts { get; set; }
+    }
+}
---
+++ Fasterr.Data.Models/Product.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using Microsoft.EntityFrameworkCore;
+
+using static Fasterr.Common.EntityValidationConstants.Product;
+
+namespace Fasterr.Data.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+            Id = Guid.NewGuid();
+            //ProductsBuyersCart = new HashSet<ProductBuyerCart>();
+        }
+
+        [Key]
+        [Comment("Product identifier")]
+        public Guid Id { get; set; }
+
+        [Required]
+        [Comment("Product name")]
+        [StringLength(NameMaxLength)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [Comment("Product description")]
+        [StringLength(DescriptionMaxLength)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        [Comment("Product image")]
+        public string ImageURL { get; set; } = string.Empty;
+
+        [Required]
+        [Comment("Product price")]
+        public decimal Price { get; set; }
+
+        [Comment("Product price discount")]
+        public int? Discount { get; set; }
+
+        [Required]
+        [Comment("Product rating")]
+        [Range(1, 5)]
+        public int Rating { get; set; }
+
+        [Required]
+        [Comment("Product category identifier")]
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; } = null!;
+
+        //public ICollection<ProductBuyerCart> ProductsBuyersCart { get; set; }
+    }
+}
