4b8dcb8be66fd764c94534a04b6cf3d9133a3eaf
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-06-04 18:08:50 +0300

    Added columns to entity tables

--- Fasterr.Data.Models/Category.cs
+++ Fasterr.Data.Models/Category.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using static Fasterr.Common.EntityValidationConstants.Category;
+
+using Microsoft.EntityFrameworkCore;
 
 namespace Fasterr.Data.Models
 {
     public class Category
     {
+        [Key]
+        [Comment("Category identifier")]
+        public int Id { get; set; }
 
+        [Required]
+        [StringLength(NameMaxLength)]
+        public string Name { get; set; } = string.Empty;
     }
 }
--- Fasterr.Data.Models/ProductBuyerPurchased.cs
+++ Fasterr.Data.Models/ProductBuyerPurchased.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
 
 namespace Fasterr.Data.Models
 {
     public class ProductBuyerPurchased
     {
+        [Required]
+        [Comment("User identifier")]
+        public Guid BuyerId { get; set; }
+
+        public ApplicationUser Buyer { get; set; } = null!;
+
+        [Required]
+        [Comment("Product identifier")]
+        public Guid ProductId { get; set; }
+
+        public Product Product { get; set; } = null!;
     }
 }
