091332a8e709b2e20c9f118ab400bfb24692cfe5
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-06-04 18:09:38 +0300

    Made extension for DbContext and Identity

---
+++ Fasterr.Web.Infrastructure/ServiceCollectionExtension.cs
@@ -0,0 +1,44 @@
+﻿using Fasterr.Data;
+using Fasterr.Data.Models;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fasterr.Web.Infrastructure
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<FasterrDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDefaultIdentity<ApplicationUser>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireDigit = false;
+            })
+                .AddRoles<IdentityRole<Guid>>()
+                .AddEntityFrameworkStores<FasterrDbContext>();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Lockout.AllowedForNewUsers = false;
+            });
+
+            return services;
+        }
+    }
+}
