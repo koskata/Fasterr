43c5fe20cbefa49831ebdd6e2159d650aa421489
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-06-07 17:01:29 +0300

    Made Product controller and views for it

---
+++ Fasterr/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Fasterr.Services.Interfaces;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fasterr.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService productService;
+
+        public ProductController(IProductService _productService)
+        {
+            productService = _productService;
+        }
+
+        public async Task<IActionResult> All()
+        {
+            var products = await productService.GetAllProductsAsync();
+
+            return View(products);
+        }
+    }
+}
