23c4343d3e095bf54addcaef42dc7a2e86b561b8
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-06-07 17:02:31 +0300

    Created services for product controller and view models

---
+++ Fasterr.Services/ProductService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Fasterr.Data;
+using Fasterr.Services.Interfaces;
+using Fasterr.Web.ViewModels.Product;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Fasterr.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly FasterrDbContext context;
+
+        public ProductService(FasterrDbContext _context)
+        {
+            context = _context;
+        }
+
+        public async Task<List<ProductAllViewModel>> GetAllProductsAsync()
+        {
+            var products = await context.Products
+                .Select(x => new ProductAllViewModel()
+                {
+                    Id = x.Id.ToString(),
+                    Name = x.Name,
+                    Description = x.Description,
+                    ImageURL = x.ImageURL,
+                    Price = x.Price,
+                    Discount = x.Discount,
+                    Rating = x.Rating,
+                    Brand = x.Brand,
+                    Category = x.Category.Name
+                }).ToListAsync();
+
+            return products;
+        }
+    }
+}
---
+++ Fasterr.Web.ViewModels/Product/ProductAllViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static Fasterr.Common.EntityValidationConstants.Product;
+
+namespace Fasterr.Web.ViewModels.Product
+{
+    public class ProductAllViewModel
+    {
+        public string Id { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(NameMaxLength)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(DescriptionMaxLength)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public string ImageURL { get; set; } = string.Empty;
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public int? Discount { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int Rating { get; set; }
+
+        [Required]
+        public string Brand { get; set; } = string.Empty;
+
+        [Required]
+        public string Category { get; set; } = string.Empty;
+    }
+}
