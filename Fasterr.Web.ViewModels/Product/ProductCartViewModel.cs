416e50eaa7bf166c657ba8088336fc6774584674
koskata <koskata@students.softuni.bg>
2024-06-17 15:37:28 +0300

    Created view model for cart product entities

---
+++ Fasterr.Web.ViewModels/Product/ProductCartViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static Fasterr.Common.EntityValidationConstants.Product;
+
+namespace Fasterr.Web.ViewModels.Product
+{
+    public class ProductCartViewModel
+    {
+        public string Id { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(NameMaxLength)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(DescriptionMaxLength)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public string ImageURL { get; set; } = string.Empty;
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public int? Discount { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int Rating { get; set; }
+
+        //[Required]
+        //public string Brand { get; set; } = string.Empty;
+
+        //[Required]
+        //public string Category { get; set; } = string.Empty;
+
+        [Required]
+        public string Type { get; set; } = string.Empty;
+    }
+}
