3363710c37c6333b66d14778efc1bb074c56cc47
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-07-03 16:19:00 +0300

    Created controller for profile and base view for it

---
+++ Fasterr/Controllers/ProfileController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fasterr.Controllers
+{
+    [Authorize]
+    public class ProfileController : Controller
+    {
+        public IActionResult Info()
+        {
+            return View();
+        }
+    }
+}
