a70c1e839626eb8bd06c78cbddef05eb5edae88e
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-07-09 15:21:55 +0300

    Created controller for modal content action

---
+++ Fasterr/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Fasterr.Services.Interfaces;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fasterr.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IProfileService profileService;
+
+        public UserController(IProfileService _profileService)
+        {
+            profileService = _profileService;
+        }
+
+        public async Task<IActionResult> ModalContent(string userId)
+        {
+            if (userId == null)
+            {
+                return BadRequest();
+            }
+
+
+            var likedProducts = await profileService.GetAllLikedProductsByUserAsync(userId);
+
+            return PartialView("~/Views/Shared/_ModalContent.cshtml", likedProducts);
+        }
+    }
+}
