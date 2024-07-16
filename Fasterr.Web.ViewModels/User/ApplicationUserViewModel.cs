5b0277180478d7c75f109d6613b4f7e27fae00d3
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-07-16 18:29:39 +0300

    Created view model for application user properties

---
+++ Fasterr.Web.ViewModels/User/ApplicationUserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static Fasterr.Common.EntityValidationConstants.ApplicationUser;
+
+namespace Fasterr.Web.ViewModels.User
+{
+    public class ApplicationUserViewModel
+    {
+        [Required]
+        public string Id { get; set; } = string.Empty;
+
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        public string ImageURL { get; set; } = string.Empty;
+    }
+}
