cd222f25497062d5d786c0d4565262bb7b821084
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-07-31 12:22:35 +0300

    Created controller for cart and implemented some actions from product controller

---
+++ Fasterr/Controllers/CartController.cs
@@ -0,0 +1,54 @@
+﻿using Fasterr.Services;
+using Fasterr.Services.Interfaces;
+using Fasterr.Web.Infrastructure;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fasterr.Controllers
+{
+    [Authorize]
+    public class CartController : Controller
+    {
+        private readonly ICartService cartService;
+        private readonly IProductService productService;
+
+        public CartController(ICartService _cartService, IProductService _productService)
+        {
+            cartService = _cartService;
+            productService = _productService;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Cart()
+        {
+            string userId = User.GetById();
+
+            var products = await cartService.GetAllProductsInCartAsync(userId);
+
+            return View(products);
+        }
+
+
+        [Authorize]
+        public async Task<IActionResult> RemoveFromCart(string id)
+        {
+            if (!await productService.ProductExistsByIdAsync(id))
+            {
+                return BadRequest();
+            }
+
+            string userId = User.GetById();
+
+            await cartService.RemoveProductFromCartAsync(userId, id);
+
+            return RedirectToAction(nameof(Cart));
+        }
+
+        [Authorize]
+        public IActionResult Payment()
+        {
+            return View();
+        }
+    }
+}
