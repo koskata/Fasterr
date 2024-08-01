5601cff066076525252ed070d98f2909f08f53e9
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-08-01 14:05:34 +0300

    Made new class for error message constants

---
+++ Fasterr.Common/ErrorMessages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fasterr.Common
+{
+    public static class ErrorMessages
+    {
+        public const string RequiredMessage = "The field {0} is required!";
+        public const string LengthMessage = "The field {0} must be between {2} and {1} characters long!";
+    }
+}
