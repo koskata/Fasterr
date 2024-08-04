64888624edf6b976ccbb696ae1fcd74766c04b76
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-08-04 19:53:22 +0300

    Created view model for pay with cash

---
+++ Fasterr.Web.ViewModels/Cart/PayWithCashViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static Fasterr.Common.EntityValidationConstants.PayWithCardOrCash;
+using static Fasterr.Common.ErrorMessages;
+
+namespace Fasterr.Web.ViewModels.Cart
+{
+    public class PayWithCashViewModel
+    {
+        [Display(Name = "Phone Number")]
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(PhoneNumberMaxLength, MinimumLength = PhoneNumberMinLength, ErrorMessage = LengthMessage)]
+        public string PhoneNumber { get; set; } = null!;
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(AddressMaxLength, MinimumLength = AddressMinLength, ErrorMessage = LengthMessage)]
+        public string Address { get; set; } = null!;
+
+        public decimal? TotalAmount { get; set; }
+    }
+}
