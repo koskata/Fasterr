44fbfaaead1ccb75c30234bc04409af69de27c61
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-08-06 15:33:51 +0300

    Added function for toastr success and error

--- Fasterr/wwwroot/js/site.js
+++ Fasterr/wwwroot/js/site.js
@@ -1,4 +1,33 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var message = function () {
 
-// Write your JavaScript code.
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-center",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+
+    var showSuccess = function(message) {
+        toastr["success"](message);
+    }
+
+    var showError = function (message) {
+        toastr["error"](message);
+    }
+
+    return {
+        showSuccess,
+        showError
+    }
+}();
