5e47df6925ffb6ae72b4d5bc5b4051dfa13bbfcf
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-08-06 15:35:54 +0300

    Created class for message constants

---
+++ Fasterr.Common/MessageConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fasterr.Common
+{
+    public static class MessageConstants
+    {
+        public const string UserMessageSuccess = "UserMessageSuccess";
+
+        public const string UserMessageError = "UserMessageError";
+    }
+}
